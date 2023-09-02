1e7c7bf7056f25921697bf161f90e5ab0b96a1b7
Breno W. de Assis <82486373+breno-wiebbelling@users.noreply.github.com>
2023-09-01 21:32:19 -0300

    Update baseClient.js

--- src/service/clients/baseClient.js
+++ src/service/clients/baseClient.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const baseInstance =  axios.create({
-  baseURL: "http://localhost:8080",
+  // baseURL: "http://localhost:8080",
+  baseURL: "70ebe86d1b8da.vercel.app",
   headers: { 
     "Content-Type": "application/json" 
   },
