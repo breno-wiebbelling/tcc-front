040a76a17d152b391e441d33684c343d8cd521fa
Breno Wiebbelling <brenowdea@gmail.com>
2024-04-09 21:41:52 -0300

    kjh

--- src/service/clients/baseClient.js
+++ src/service/clients/baseClient.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 import { getToken } from '../authService'
 
+const BASE_URL_HOST = "https://tcc-back-end.vercel.app"
+//http://localhost:8080",
+
 const baseInstance =  axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: BASE_URL_HOST,
   headers: { 
     "Content-Type": "application/json",
   },
